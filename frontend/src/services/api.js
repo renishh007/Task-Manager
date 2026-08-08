e098d76dd4395e403fc882102c694533fbce85a0
Renish savaliya <renishsavaliya66@gmail.com>
2026-08-08 17:03:36 +0530

    Fix API base URL to include '/api' endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-qhgi.onrender.com",
+  baseURL: "https://task-manager-qhgi.onrender.com/api",
 });
 
 export const getTasks = () => {
